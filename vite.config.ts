08dca91cab43779d032e90e0b4047e0aac9363a4
KarolKarnas <karol.karnas@gmail.com>
2024-09-08 12:45:27 +0200

    fix deploy error4?

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     host: true,
     strictPort: true,
-    port: 8000,
+    port: Number(process.env.PORT) || 8000,
   },
   test: {
     globals: true,
